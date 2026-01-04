3a9de5fe912c07442089cd39a6c5b70f3a8249fb
Aakash Faliya <aakashfaliya1@gmail.com>
2026-01-04 10:18:42 +0000

    still signin faild error comes.why it comes?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,7 +15,7 @@ const firebaseConfig: FirebaseOptions = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
