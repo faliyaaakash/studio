2da27c3a7e524d76604af21093ca2b7bbe93d39f
Aakash Faliya <aakashfaliya1@gmail.com>
2026-01-04 10:13:52 +0000

    sign in is failed ? check why it is failed?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
-  projectId: "studio-7838183676-b74cb",
-  appId: "1:1069984663146:web:db6ca5e5875e00cc2bec13",
-  apiKey: "AIzaSyD5oLneuuqwYlLlIZsCFSSBl1KWPX1n8Ac",
-  authDomain: "studio-7838183676-b74cb.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "1069984663146",
-  storageBucket: "studio-7838183676-b74cb.appspot.com"
+    apiKey: "AIzaSyD5oLneuuqwYlLlIZsCFSSBl1KWPX1n8Ac",
+    authDomain: "studio-7838183676-b74cb.firebaseapp.com",
+    projectId: "studio-7838183676-b74cb",
+    storageBucket: "studio-7838183676-b74cb.appspot.com",
+    messagingSenderId: "1069984663146",
+    appId: "1:1069984663146:web:db6ca5e5875e00cc2bec13",
+    measurementId: ""
 };
 
 // Initialize Firebase
