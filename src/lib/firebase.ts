607bfe4d51247b2ebd29330277b5eb2c43aa8136
faliyaaakash <aakashfaliya1@gmail.com>
2026-01-05 17:21:13 +0530

    final commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: "AIzaSyD5oLneuuqwYlLlIZsCFSSBl1KWPX1n8Ac",
-    authDomain: "studio-7838183676-b74cb.firebaseapp.com",
-    projectId: "studio-7838183676-b74cb",
-    storageBucket: "studio-7838183676-b74cb.appspot.com",
-    messagingSenderId: "1069984663146",
-    appId: "1:1069984663146:web:db6ca5e5875e00cc2bec13",
-    measurementId: ""
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
